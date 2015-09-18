118b0aee4e2d3813c9557711c5dd34b40a3beb04
Tocknicsu <wingemerald@gmail.com>
2015-09-18 15:00:09 +0800

    update

---
+++ zj/b541.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str[42];
+    str[1] = "1";
+    for(int i = 2 ; i < 41 ; i++){
+        int x = 0, now = 0;
+        for(int j = 0 ; j < (int)str[i-1].size() ; j++){
+        }
+    }
+
+    return 0;
+}
