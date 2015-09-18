2fa0fe43c5f62288f2034fb4e65268902e490353
Tocknicsu <wingemerald@gmail.com>
2015-09-18 09:21:49 +0800

    update

--- pcca/2015_summer/0707/basic/pa.cpp
+++ pcca/2015_summer/0707/basic/pa.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <iomanip>
 using namespace std;
 double p, a, b, c, d, n, ans;
 int main(){
     ios_base::sync_with_stdio(0), cin.tie(0);
-    cin >> p >> a >> b >> c >> d >> n;
-    vector<double> v(n), tmp(n);
-    tmp[0] = -1029348576;
-    for(int i = 1 ; i <= n ; i++){
-        v[i] = p * (sin(a * double(i) + b) + cos(c * double(i) + d) + 2);
-        cout << v[i] << endl;
-        tmp[i] = v[i] > v[i-1] ? 0 : tmp[i-1] + v[i] - v[i-1];
-        ans = max(ans, tmp[i]);
+    while(cin >> p >> a >> b >> c >> d >> n){
+        vector<double> v;
+        for(int i = 1 ; i <= n ; i++)
+            v.push_back(p * (sin(a * double(i) + b) + cos(c * double(i) + d) + 2));
+        double ans = 0;
+        int l = 0;
+        for(int i = 1 ; i < n ; i++){
+            if(v[i] > v[l]){
+                l = i;
+            } else {
+                ans = max(ans, v[l] - v[i]);
+            }
+        }
+        cout << fixed << setprecision(6) << ans << endl;
     }
-    cout << ans << endl;
     return 0;
 }
