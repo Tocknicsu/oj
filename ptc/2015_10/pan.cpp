363ff2599a448c30f239c4ad49c9225dfb11a31c
Tocknicsu <wingemerald@gmail.com>
2015-10-14 19:19:37 +0800

    update

--- ptc/2015_10/pan.cpp
+++ ptc/2015_10/pan.cpp
@@ -21,12 +21,17 @@ void Solve(){
     vector<int> x, y;
     vector<S> s;
     int sx, sy, ex, ey;
+    int nsx, nsy, nex, ney;
     for(int i = 0 ; i < n ; i++){
         scanf("%d%d%d%d", &sx, &sy, &ex, &ey);
+        nsx = min(sx, ex);
+        nex = max(sx, ex);
+        nsy = min(sy, ey);
+        ney = max(sy, ey);
         x.push_back(sx); x.push_back(ex);
         y.push_back(sy); x.push_back(ey);
-        s.push_back(S(1, sx, sy, ey));
-        s.push_back(S(-1, ex, sy, ey));
+        s.push_back(S(1, nsx, nsy, ney));
+        s.push_back(S(-1, nex, nsy, ney));
     }
     sort(ALL(x)); sort(ALL(y));
     x.resize(unique(ALL(x))-x.begin());
