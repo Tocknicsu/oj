1359ae99de010e1d23e0f45b7b4c6f074838f6cc
Tocknicsu <wingemerald@gmail.com>
2015-10-22 23:35:54 +0800

    update

--- lab/104_1/dlab/hw3/hw3.v
+++ lab/104_1/dlab/hw3/hw3.v
@@ -1,30 +1,52 @@
+`timescale 1ns / 1ps
+
 module lab3(
-    input wire CLK,
-    input wire reset,
-    input wire btn_east,
-    input wire btn_west,
-    output wire [7:0] led
+    input  clk,
+    input  reset,
+    input  btn_east,
+    input  btn_west,
+    output [7:0] led
 );
 
-integer east_count, west_count;
-reg [7:0] value;
+integer east_count, west_count, east_down, west_down;
+reg signed [7:0] value;
 assign led=value;
 
-always @(posedge CLK)
+
+always @(posedge clk)
 begin
-    if(reset)
-        value <= 0
-    if(btn_east)
-        east_count <= east_count + 1
-    else
-        east_count <= 0
-    if(west_count)
-        west_count <= west_count + 1
-    else
-        west_count <= 0
-    if(west_count == 23456 & value < 7)
-        value <= value + 1
-    if(east_count == 23456 & value > -8)
-        value <= value - 1
+    if(reset) begin
+        value <= 0;
+        east_count <= 0;
+        east_down <= 0;
+        west_count <= 0;
+        west_down <= 0;
+    end else begin
+        if(btn_east)
+            east_count <= east_count + 1;
+        else begin
+            east_count <= 0;
+            east_down <= 0;
+        end
+
+        if(btn_west)
+            west_count <= west_count + 1;
+        else begin
+            west_count <= 0;
+            west_down <= 0;
+        end
+
+        if(west_count == 123456 & value < 7 & !west_down) begin
+            value <= value + 1;
+            west_down <= 1;
+        end else begin
+		  end
+        if(east_count == 123456 & value > -8 & !east_down) begin
+            value <= value - 1;
+            east_down <= 1;
+        end else begin
+		  end
+    end
 end
+endmodule
 
