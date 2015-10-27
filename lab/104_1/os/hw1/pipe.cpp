d0aec64f9baf77839c99f5215dfa64927e7345b8
Tocknicsu <wingemerald@gmail.com>
2015-10-28 01:23:28 +0800

    update

---
+++ lab/104_1/os/hw1/pipe.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str;
+    int cnt = 0;
+    while(cin >> str){
+        cout << cnt++ << ' ' << str << endl;
+    }
+    return 0;
+}
---
+++ lab/104_1/os/hw1/test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    int start = 0;
+    int end = 100;
+    for(int i = start ; i < end ; i++){
+        cout << i << endl;
+        sleep(1);
+    }
+    return 0;
+}
