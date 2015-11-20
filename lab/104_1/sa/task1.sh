6ba09d1787203c4c9536741c02d7fd329af6a3bf
Chun-Kai Chen <ckchen0811@bsd1.cs.nctu.edu.tw>
2015-11-21 03:57:42 +0800

    update

--- lab/104_1/sa/task1.sh
+++ lab/104_1/sa/task1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ls -al|awk 'BEGIN{printf"0directory:\n2files:\n4total:\n5\t";system("du -shc|sed -e 1d")}NR<=3{next}/^d/{printf"1\t"}/^-/{printf"3\t"}/^[d-]/{$9;c=sprintf("du -sh %s",$9);system(c)}'|sort -k1h -k2hr|sed -e"s/^.//"
+ls -al|awk 'BEGIN{printf"0directory:\n2file:\n4total:\n5\t";system("du -shc|sed -e 1d")}NR<4{next}/^d/{printf"1\t"}/^-/{printf"3\t"}/^[d-]/{$9;c=sprintf("du -sh %s",$9);system(c)}'|sort -k1h -k2hr|sed -e"s/^.//"
