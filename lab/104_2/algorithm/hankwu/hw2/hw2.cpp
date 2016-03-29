9aaceda4244eea18ed32a08f5af231c629b7a6e5
Tocknicsu <wingemerald@gmail.com>
2016-03-30 00:32:06 +0800

    hw2 complete

---
+++ lab/104_2/algorithm/hankwu/hw2/hw2.cpp
@@ -0,0 +1,40 @@
+#include <msgpack.hpp>
+#include <msgpack/vrefbuffer.hpp>
+#include <vector>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+void Read(msgpack::unpacker &unpacker, string filename){
+    FILE *fpIn = fopen(filename.c_str(), "rb");
+    fseek(fpIn, 0, SEEK_END);
+    int filesize = ftell(fpIn);
+    rewind(fpIn);
+
+    unpacker.reserve_buffer(filesize);
+    fread(unpacker.buffer(), filesize, 1, fpIn);
+    unpacker.buffer_consumed(filesize);
+    fclose(fpIn);
+}
+void Write(msgpack::sbuffer &sbuf, string filename){
+    FILE *fpOut = fopen(filename.c_str(), "wb");
+    fwrite(sbuf.data(), sbuf.size(), 1, fpOut);
+    fclose(fpOut);
+}
+
+int main(){
+    msgpack::unpacker unpacker;
+    msgpack::unpacked result;
+    msgpack::sbuffer sbuf;
+    Read(unpacker, "input.txt");
+    unpacker.next(&result);
+    vector<int> vec;
+    while(unpacker.next(&result)){
+        result.get().convert(vec);
+        sort(vec.begin(), vec.end());
+        pack(&sbuf, vec);
+    }
+    Write(sbuf, "output.txt");
+    return 0;
+}
+
